aaf2c49fe2f80bf20926baaf7b1daeb145bdbc7c
Daou Ahmed Amine <uapv1500065@ecv-2w26-105.univ-avignon.fr>
2018-05-09 14:57:25 +0200

    mock Ianimal

--- src/test/java/fr/univavignon/rodeo/api/IAnimalTest.java
+++ src/test/java/fr/univavignon/rodeo/api/IAnimalTest.java
@@ -1,13 +1,34 @@
 package fr.univavignon.rodeo.api;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import org.junit.Test;
 
 
 public class IAnimalTest  {
+	
+	public static IAnimal createMock(int xp, boolean secret, boolean endagered, boolean boss) {
+		IAnimal animalMock = Mockito.mock(IAnimal.class);
+		Mockito.when(animalMock.getXP()).thenReturn(xp);
+		Mockito.when(animalMock.isSecret()).thenReturn(secret);
+		Mockito.when(animalMock.isEndangered()).thenReturn(endagered);
+		Mockito.when(animalMock.isBoss()).thenReturn(boss);
+		return animalMock;
+}
+	protected IAnimal createMock(){
+		return createMock(1899,true,false,true);
+	
+		
+	}
+		
 
 	@Test
 	public void testgetXP(){
-		
+		IAnimal mockAnimal=createMock();
+		assertEquals(1899,mockAnimal.getXP());
 		
 	}
 	
@@ -16,11 +37,14 @@ public class IAnimalTest  {
 		
 		
 	}
+	
+	
 	@Test
 	public void testIsEndangered(){
 		
 		
 	}
+	
 	@Test
 	public void testIsSecret(){
 		
