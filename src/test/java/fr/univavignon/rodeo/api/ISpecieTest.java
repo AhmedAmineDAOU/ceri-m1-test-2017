d89cf61086fe0717325d95f1235424f4b64001e2
Daou Ahmed Amine <uapv1500065@ecv-2w26-105.univ-avignon.fr>
2018-05-09 12:35:47 +0200

    Calhanoglu

--- src/test/java/fr/univavignon/rodeo/api/ISpecieTest.java
+++ src/test/java/fr/univavignon/rodeo/api/ISpecieTest.java
@@ -1,27 +1,10 @@
 package fr.univavignon.rodeo.api;
 
-import java.util.List;
 
-/**
- * Specie definition.
- * 
- * @author fv
- */
-public interface ISpecieTest extends INamedObject {
 
-	/**
-	 * Getter for the target area in which this
-	 * specie can be found.
-	 * 
-	 * @return The area in which this specie can be found from.
-	 */
-	int getArea();
 
-	/**
-	 * Associated animals getter.
-	 * 
-	 * @return List of available animal for this specie.
-	 */
-	List<IAnimal> getAnimals();
+public class ISpecieTest  {
+
+	
 
 }
