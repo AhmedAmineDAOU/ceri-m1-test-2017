fa23125e6d5d9b310976fe1e6afc45a6cff36e2c
AhmedAmineDAOU <ahmedaminedaou@gmail.com>
2018-05-09 17:56:08 +0200

    message

--- src/test/java/fr/univavignon/rodeo/api/IGameStateTest.java
+++ src/test/java/fr/univavignon/rodeo/api/IGameStateTest.java
@@ -1,24 +1,40 @@
 package fr.univavignon.rodeo.api;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+import fr.univavignon.rodeo.api.IGameState;
 
 
 public class IGameStateTest {
 
+	public static IAnimal iAnimal = IAnimalTest.createMock(1, true, true, true);
+	public IGameState iGameState =createMock();
+	
+	public static IGameState  createMock() {
+		
+		IGameState iGameState = Mockito.mock(IGameState.class);
+		Mockito.doThrow(new IllegalStateException()).when(iGameState).exploreArea();
+		Mockito.doThrow(new IllegalArgumentException()).when(iGameState).catchAnimal(null);
+		Mockito.doThrow(new IllegalStateException()).when(iGameState).catchAnimal(iAnimal);
+		return iGameState;
+	}
+	
+	
+	
 	@Test
-	public void testCatchAnimal(){
+	public void testgetProgession(){
 		
+		when(iGameState.getProgression()).thenReturn(1);
+		assertEquals(1, iGameState.getProgression());
 	}
 	
-	
-	@Test
-	public void testGetProgression(){
-		
-	}
-	
-	@Test 
-	public void testGetSpeciesLevel(){
-		
-	}
+/*	@Test
+	public void testSpecialLevel(){
+		ISpecie iSpecie = ISpecieTest.getISpecieTest();
+		when(iGameState.getSpecieLevel(iSpecie)).thenReturn(SpecieLevel.CHAMPION);
+		assertEquals(SpecieLevel.CHAMPION, iGameState.getSpecieLevel(iSpecie));
+}*/
 
 }
