304bf5772af19b42f7508aa74eec6e6d7a2fe072
AhmedAmineDAOU <ahmedaminedaou@gmail.com>
2018-05-09 18:43:34 +0200

    message

--- src/test/java/fr/univavignon/rodeo/api/IEnvironmentProviderTest.java
+++ src/test/java/fr/univavignon/rodeo/api/IEnvironmentProviderTest.java
@@ -1,22 +1,49 @@
 package fr.univavignon.rodeo.api;
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import fr.univavignon.rodeo.api.IEnvironment;
+import fr.univavignon.rodeo.api.IEnvironmentProvider;
 
 public class IEnvironmentProviderTest {
 	
+	private static List<String> environmentsList;
+	private static IEnvironment environment;
+	
+	public static IEnvironmentProvider getTestInstanceStatic() {
+		
+		IEnvironmentProvider environmentProviderTest = Mockito.mock(IEnvironmentProvider.class);
+		
+		environmentsList = new LinkedList<String>();
+		environmentsList.add("list");
+		
+		Mockito.when(environmentProviderTest.getAvailableEnvironments()).thenReturn(environmentsList);
+		
+		environment = IEnvironmentTest.getTestInstanceStatic(4);
+		
+		Mockito.when(environmentProviderTest.getEnvironment("name")).thenReturn(environment);
+		Mockito.when(environmentProviderTest.getEnvironment(null)).thenThrow(new IllegalArgumentException());
+		
+		
+		return environmentProviderTest;
+	}
+	
+	
 	@Test
-	public void testGetAvailableEnvironments(){
-		
-		
+	public void testGetAvailableEnvironments() {
+		IEnvironmentProvider test = getTestInstanceStatic();
+		assertEquals(test.getAvailableEnvironments(),environmentsList);
 	}
 	
 	@Test
-	public void testGetEnvironment(){
-		
-		
+	public void testgetEnvironment() {
+		IEnvironmentProvider test = getTestInstanceStatic();
+		assertEquals(test.getEnvironment("name"),environment);
 	}
-	
-	
 
 }
--- src/test/java/fr/univavignon/rodeo/api/ISpecieTest.java
+++ src/test/java/fr/univavignon/rodeo/api/ISpecieTest.java
@@ -1,19 +1,48 @@
 package fr.univavignon.rodeo.api;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import fr.univavignon.rodeo.api.IAnimal;
+import fr.univavignon.rodeo.api.ISpecie;
 
-
-public class ISpecieTest  {
-
-	@Test
-	public void testGetAnimals(){
+public class ISpecieTest {
+	
+	
+	private static List<IAnimal> animalsList;
+	public static ISpecie getTestInstanceStatic(int area) {
 		
+		ISpecie specieMock = Mockito.mock(ISpecie.class);
+		Mockito.when(specieMock.getArea()).thenReturn(area);
+		
+		animalsList = new LinkedList<IAnimal>();
+		animalsList.add(IAnimalTest.createMock(2, true, false, false));
+		animalsList.add(IAnimalTest.createMock(4, true, true, false));
+		
+		Mockito.when(specieMock.getAnimals()).thenReturn(animalsList);
+		
+		return specieMock;
+	}
+	protected ISpecie getTestInstance() {
+		
+		return getTestInstanceStatic(3);
+	
 	}
 	
 	@Test
-	public void testGetArea(){
-		
+	public void testGetArea() {
+		ISpecie test = getTestInstance();
+		assertEquals(test.getArea(),3);
 	}
-
+	
+	@Test
+	public void testGetAnimals() {
+		ISpecie test = getTestInstance();
+		assertEquals(test.getAnimals(),animalsList);
+	}
 }
