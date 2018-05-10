aba946161589933d8b9baf32af7ba0c80e7c371f
AhmedAmineDAOU <ahmedaminedaou@gmail.com>
2018-05-10 15:31:51 +0200

    add

---
+++ src/main/java/fr/univavignon/rodeo/implementation/NamedObject.java
@@ -0,0 +1,12 @@
+package fr.univavignon.rodeo.implementation;
+
+import fr.univavignon.rodeo.api.INamedObject;
+
+public class NamedObject  implements INamedObject {
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
